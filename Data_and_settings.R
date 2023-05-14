e8f6ddec90fa1b93cbaa8c4afa67692a72bf36c3
Mariana <marianavelez.35@gmail.com>
2023-05-13 19:15:15 -0600

    commit

---
+++ Data_and_settings.R
@@ -0,0 +1,21 @@
+# Packages needed #
+library(tidyverse)
+library(showtext)
+library(ggpubr)
+library(patchwork)
+library(readxl)
+library(gt)
+
+
+# Data import #
+
+datos_TS <- read_excel("datos_TS.xlsx")
+
+datos_TS<-datos_TS[,1:6] # Deleting the mean and median table included in the Excel
+# Checking spelling and blankspaces 
+names(datos_TS) #Checked
+unique(datos_TS$Album) #Checked
+unique(datos_TS$Skip) #Checked
+unique(datos_TS$Letra) #Checked
+unique(datos_TS$Meidentifico) #Checked
+
